95a8c08025b00bace96ac073fcdbae9aeb3d3f5e
Ikram bargaoui <ikram.bargaoui@esprit.tn>
2023-10-25 23:26:23 +0100

    frontend

---
+++ src/app/models/CandidacyModule/Candidacy.module.ts
@@ -0,0 +1,28 @@
+import { User } from '../UserModule/User.module';
+import { Offer } from '../OfferModule/Offer.module';
+import { StatusCandidacy } from '../CandidacyModule/StatusCandidacy';
+import { Profil } from '../OfferModule/Profil';
+import { Disponibilite } from '../CandidacyModule/Disponibilite';
+import { DomainCandidacy } from '../CandidacyModule/DomainCandidacy';
+export class Candidacy {
+    idCandidacy!: number;
+    cv!: string;
+    coverLetter!: string;
+    certificate!: string;
+    firstName!: string;
+    lastName!: string;
+    email!: string; 
+    dateCandidacy!: Date;
+    telephoneNumber!: number;
+    address!: string;
+    postalCode!: number;
+    moyenneGenerale!:number;
+    scoree!: number;
+    anneeExperience!: number;
+    domainCandidacy!: DomainCandidacy;
+    statusCandidacy!: StatusCandidacy;
+    disponibilite!: Disponibilite;
+   
+
+
+  }
